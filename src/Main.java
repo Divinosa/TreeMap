9af023a20a8be753c4f3a39d0fba7529d3be35e5
“Danila” <Danilabirulevo@gmail.com>
2023-03-21 23:50:36 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Oleg","Popov", 25));
         people.add(new Person("Ivan","Kolit", 30));
-        people.add(new Person("Aleksandr","Nevskiy", 35));
         people.add(new Person("Yuriy","Dolgorukiy", 40));
-        Collections.sort(people, new ComparatorPerson());
+        people.add(new Person("Aleksandr","Nevskiy", 35));
+        people.sort((o1, o2) -> {if (o1.getSurname().length() > o2.getSurname().length()){
+            return 1;
+        } else if (o1.getSurname().length() < o2.getSurname().length()) {
+            return -1;
+        } else {
+            return o1.getAge() - o2.getAge();
+        }});
         System.out.println(people);
     }
 }
